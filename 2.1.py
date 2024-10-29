f1784510c234e9aefce91c8cf9ad8ce5960a191f
userMa1009 <kovalevamaria1009@gmail.com>
2024-10-29 18:51:01 +0700

    Add files via upload

---
+++ 2.1.py
@@ -0,0 +1,9 @@
+first=3
+second=4
+third =6
+if first==second and first==third and second==third:
+    print(3)
+elif first==second or first==third or second==third:
+    print(2)
+else:
+    print(0)
